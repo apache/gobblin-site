27ec9f6ee772840afcf5561e8d2624b509c9a6a0
Abhishek Tiwari <abhishektiwari.btech@gmail.com>
2018-07-02 21:41:03 +0530

    Add release 0.12.0 details, simplify website modification for subsequent releases

---
+++ assets/js/gobblin.js
@@ -0,0 +1,7 @@
+$(document).ready(function(e) {
+    // For all other pages
+    $('#navigation').load('../navigation.html',function(){});
+    
+    // For home page
+    $('#navigationHomePage').load('../navigation.html',function(){});
+});
